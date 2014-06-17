a7c99a9e01096b9431c94ec6ad518e2c5ef77992
Cedric Stoquer <cstoquer@wizcorp.jp>
2014-06-17 20:51:19 +0900

    style

--- main.cpp
+++ main.cpp
@@ -9,11 +9,9 @@
 #include <SDL.h>
 #endif
 
-int main ( int argc, char** argv )
-{
+int main ( int argc, char** argv ) {
     // initialize SDL video
-    if ( SDL_Init( SDL_INIT_VIDEO ) < 0 )
-    {
+    if ( SDL_Init( SDL_INIT_VIDEO ) < 0 ) {
         printf( "Unable to init SDL: %s\n", SDL_GetError() );
         return 1;
     }
@@ -22,18 +20,15 @@ int main ( int argc, char** argv )
     atexit(SDL_Quit);
 
     // create a new window
-    SDL_Surface* screen = SDL_SetVideoMode(640, 480, 16,
-                                           SDL_HWSURFACE|SDL_DOUBLEBUF);
-    if ( !screen )
-    {
+    SDL_Surface* screen = SDL_SetVideoMode(640, 480, 16, SDL_HWSURFACE | SDL_DOUBLEBUF);
+    if (!screen) {
         printf("Unable to set 640x480 video: %s\n", SDL_GetError());
         return 1;
     }
 
     // load an image
     SDL_Surface* bmp = SDL_LoadBMP("cb.bmp");
-    if (!bmp)
-    {
+    if (!bmp) {
         printf("Unable to load bitmap: %s\n", SDL_GetError());
         return 1;
     }
@@ -45,15 +40,12 @@ int main ( int argc, char** argv )
 
     // program main loop
     bool done = false;
-    while (!done)
-    {
+    while (!done) {
         // message processing loop
         SDL_Event event;
-        while (SDL_PollEvent(&event))
-        {
+        while (SDL_PollEvent(&event)) {
             // check for messages
-            switch (event.type)
-            {
+            switch (event.type) {
                 // exit if the window is closed
             case SDL_QUIT:
                 done = true;
@@ -61,12 +53,9 @@ int main ( int argc, char** argv )
 
                 // check for keypresses
             case SDL_KEYDOWN:
-                {
-                    // exit if ESCAPE is pressed
-                    if (event.key.keysym.sym == SDLK_ESCAPE)
-                        done = true;
-                    break;
-                }
+                // exit if ESCAPE is pressed
+                if (event.key.keysym.sym == SDLK_ESCAPE) done = true;
+                break;
             } // end switch
         } // end of message processing
 
