16e1b66d14567e0e1eb3d6a407e7f8be72e74855
Cedric Stoquer <cstoquer@wizcorp.jp>
2014-06-28 14:13:29 +0900

    moved OscRamp implementation to header

--- OscRamp.h
+++ OscRamp.h
@@ -1,18 +1,22 @@
 #ifndef OSC_RAMP_H
 #define OSC_RAMP_H
 #include "Oscillator.h"
+#include "constants.h"
 
+const float rate  = (float) SAMPLE_RATE;
 
 class OscRamp : public Oscillator {
+	
+private:
 
 public:
 
-	float tic();
-
-
-private:
-
-
+	float tic() {
+		pos += freq / rate;
+		if (pos > 1) pos -= 1;
+		out = 2 * pos - 1;
+		return out;
+	};
 };
 
 #endif
