5d3790085b73d69ae063549be1f43f304287bf54
Cedric Stoquer <cstoquer@wizcorp.jp>
2014-06-28 14:21:20 +0900

    moved CombFilter implementation to header

--- CombFilter.h
+++ CombFilter.h
@@ -3,21 +3,41 @@
 
 class CombFilter {
 
-public:
-
-	CombFilter();
-    float tic(float);
-
-	float out;
-	float feedbackGain;
-	float feedforwardGain;
-
 private:
 	float  delay1[100];
 	float  delay2[100];
 
 	int    posIn;
 	int    posOut;
+
+public:
+	float out;
+	float feedbackGain;
+	float feedforwardGain;
+
+	CombFilter() {
+		out             =  0;
+		feedbackGain    =  0.4;
+		feedforwardGain = -0.4;
+
+		for (int i = 0; i < 100; i++) {
+			delay1[i] = 0;
+			delay2[i] = 0;
+		}
+		posIn  = 0;
+		posOut = 1;
+	};
+
+    float tic(float in) {
+		delay2[posIn] = out;
+		out = in + feedforwardGain * delay1[posOut] + feedbackGain * delay2[posOut];
+		delay1[posIn] = in;
+		posIn++;
+		posOut++;
+		if (posIn  >= 100) posIn  = 0;
+		if (posOut >= 100) posOut = 0;
+		return out;
+	};
 };
 
 #endif
