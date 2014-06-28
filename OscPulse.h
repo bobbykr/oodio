e112d5869e6c58e7675304f57839042a03c98227
Cedric Stoquer <cstoquer@wizcorp.jp>
2014-06-28 14:09:32 +0900

    moved OscPulse implementation to header

--- OscPulse.h
+++ OscPulse.h
@@ -1,17 +1,28 @@
 #ifndef OSC_PULSE_H
 #define OSC_PULSE_H
 #include "Oscillator.h"
+#include "constants.h"
+
+const float rate = (float) SAMPLE_RATE;
 
 class OscPulse : public Oscillator {
 
-public:
-
-	OscPulse();
-    float tic();
-	float width;
-
 private:
 
+public:
+
+	float width;
+
+	OscPulse() {
+		width = 0.5;
+	};
+
+    float tic() {
+		pos += freq / rate;
+		if (pos > 1) pos -= 1;
+		out = (pos > width) ? 1 : 0;
+		return out;
+	};
 };
 
 #endif
