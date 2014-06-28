8b71fd3594aa539e13c11d3aff0f63dd4fdfe898
Cedric Stoquer <cstoquer@wizcorp.jp>
2014-06-28 14:15:07 +0900

    moved OscTri implementation to header

--- OscTri.cpp
+++ OscTri.cpp
@@ -1,15 +1,6 @@
 #include "OscTri.h"
-#include "constants.h"
 
-const float rate = (float) SAMPLE_RATE;
 
-OscTri::OscTri() {
-	width = 0.5;
-}
+OscTri::OscTri()
 
-float OscTri::tic() {
-	pos += freq / rate;
-	if (pos > 1) pos -= 1;
-	out = 1 - 2 * ((pos < width) ? pos / width : 1 - (pos - width) / (1 - width));
-	return out;
-}
+float OscTri::tic()
--- OscTri.h
+++ OscTri.h
@@ -1,17 +1,26 @@
 #ifndef OSC_TRI_H
 #define OSC_TRI_H
 #include "Oscillator.h"
+#include "constants.h"
+
+const float rate = (float) SAMPLE_RATE;
 
 class OscTri : public Oscillator {
-
-public:
-
-	OscTri();
-    float tic();
-	float width;
-
 private:
 
+public:
+	float width;
+
+	OscTri() {
+		width = 0.5;
+	};
+
+    float tic() {
+		pos += freq / rate;
+		if (pos > 1) pos -= 1;
+		out = 1 - 2 * ((pos < width) ? pos / width : 1 - (pos - width) / (1 - width));
+		return out;
+	};
 };
 
 #endif
